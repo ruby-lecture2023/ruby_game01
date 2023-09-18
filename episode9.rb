73fbd08f43c2cf918b4e67fda61974339b09d77a
Ida Ritsuka <e1902@gm.matsue-ct.jp>
2023-09-18 15:22:21 +0900

    10月～12月分です

--- episode9.rb
+++ episode9.rb
@@ -5,13 +5,14 @@ WINDOW_Y = 480
 
 # 人物の画像設定
 normal_img = Image.load("image/character/hohoemi.png") # 通常
-sekimen1_img = Image.load("image/character/sekimen01.png") # 赤面1
-sekimen2_img = Image.load("image/character/sekimen02.png") # 赤面2
+smile_img = Image.load("image/character/smile.png") # 笑顔
+kanashii_img = Image.load("image/character/kanashii.png") # 悲しい
 sekimen3_img = Image.load("image/character/sekimen03.png") # 赤面3
 iraira_img = Image.load("image/character/iraira.png") # いらいら
 
 # その他画像設定
 back_img = Image.load("image/back/kyousitu01.jpg") #教室背景
+back2_img = Image.load("image/back/snow.jpg") #教室背景
 window_img = Image.load("image/textbox/window_01.png") #メッセージボックス
 button_img = Image.load("image/textbox/button_01.png") # 選択肢
 button_hover_img = Image.load("image/textbox/button_01_hover.png") # 選択肢（選択状態）
@@ -38,24 +39,27 @@ Window.loop do
     timer += 1 if flag == 1
 
     # 背景の表示
-    Window.draw(0, 0, back_img)
+    Window.draw_scale(0, 0, back2_img, 0.18, 0.18, 0, 0)
 
     # モノローグの部分はcase文で管理
     case flag2
     when 0
+        Window.draw_scale(0, 0, back_img, 0.5, 0.5, 0, 0)
         Window.draw_scale(19, 374, window_img, 0.5, 0.5, 0, 0)
         Window.draw_font(44, 404, "-教室-", font, color:[165, 83, 126])
         if Input.mouse_push?(M_LBUTTON)
             flag2 = 1
         end
     when 1
+        Window.draw_scale(0, 0, back_img, 0.5, 0.5, 0, 0)
         Window.draw_scale(19, 374, window_img, 0.5, 0.5, 0, 0)
-        Window.draw_font(110, 374, "担任", font_name, color:[255, 255, 255])
-        Window.draw_font(44, 404, "はい、2学期も今日で終わりだが、ハメ外しすぎるなよー。\n解散!", font, color:[165, 83, 126])
+        Window.draw_font(125, 374, "担任", font_name, color:[255, 255, 255])
+        Window.draw_font(44, 404, "2学期も今日で終わりだが、ハメ外しすぎるなよー。\nはい、解散!", font, color:[165, 83, 126])
         if Input.mouse_push?(M_LBUTTON)
             flag2 = 2
         end
     when 2
+        Window.draw_scale(0, 0, back_img, 0.5, 0.5, 0, 0)
         Window.draw(125, 0, normal_img)
         Window.draw_scale(19, 374, window_img, 0.5, 0.5, 0, 0)
         Window.draw_font(110, 374, "中野黄治", font_name, color:[255, 255, 255])
@@ -64,6 +68,7 @@ Window.loop do
             flag2 =3
         end
     when 3
+        Window.draw_scale(0, 0, back_img, 0.5, 0.5, 0, 0)
         Window.draw(125, 0, normal_img)
         Window.draw_scale(19, 374, window_img, 0.5, 0.5, 0, 0)
         Window.draw_font(110, 374, "氷見鏡子", font_name, color:[255, 255, 255])
@@ -72,8 +77,6 @@ Window.loop do
             flag2 =4
         end
     when 4
-        Window.draw(125, 0, normal_img)
-        Window.draw(0, 0, back2_img)
         Window.draw_scale(19, 374, window_img, 0.5, 0.5, 0, 0)
         Window.draw_font(44, 404, "-通学路-", font, color:[165, 83, 126])
         if Input.mouse_push?(M_LBUTTON)
@@ -81,7 +84,7 @@ Window.loop do
         end
     when 5
         Window.draw(125, 0, normal_img)
-        Window.draw_scale(0, 0, window_img, 0, 0, 640, 320)
+        Window.draw_scale(19, 374, window_img, 0.5, 0.5, 0, 0)
         Window.draw_font(110, 374, "氷見鏡子", font_name, color:[255, 255, 255])
         Window.draw_font(44, 404, "ワンちゃん用の可愛いケーキがあって良かったね", font, color:[165, 83, 126])
         if Input.mouse_push?(M_LBUTTON)
@@ -152,22 +155,22 @@ Window.loop do
         # 選択肢による反応の変化と、好感度の加算
         case player_chose
         when 1 then
-            Window.draw(125, 0, normal_img)
+            Window.draw(125, 0, smile_img)
             Window.draw_scale(19, 374, window_img, 0.5, 0.5, 0, 0)
-            Window.draw_font(110, 374, "仲野央士", font_name, color:[255, 255, 255])
+            Window.draw_font(110, 374, "中野黄治", font_name, color:[255, 255, 255])
             Window.draw_font(44, 404, "わぁ、ありがとう。すごい偶然だね。\n大切に使うよ。", font, color:[165, 83, 126])
             Window.draw_font(0, 0, "好感度：#{likability}", font, color:[255, 255, 255])
         when 2 then
             Window.draw(125, 0, normal_img)
             Window.draw_scale(19, 374, window_img, 0.5, 0.5, 0, 0)
-            Window.draw_font(110, 374, "仲野央士", font_name, color:[255, 255, 255])
-            Window.draw_font(44, 404, "うん、やっぱり似合うね。", font, color:[165, 83, 126])
+            Window.draw_font(110, 374, "中野黄治", font_name, color:[255, 255, 255])
+            Window.draw_font(44, 404, "あぁ、そう…。", font, color:[165, 83, 126])
             Window.draw_font(0, 0, "好感度：#{likability}", font, color:[255, 255, 255])
         when 3 then
-            Window.draw(125, 0, normal_img)
+            Window.draw(125, 0, kanashii_img)
             Window.draw_scale(19, 374, window_img, 0.5, 0.5, 0, 0)
-            Window.draw_font(110, 374, "仲野央士", font_name, color:[255, 255, 255])
-            Window.draw_font(44, 404, "あぁ、そう…。", font, color:[165, 83, 126])
+            Window.draw_font(110, 374, "中野黄治", font_name, color:[255, 255, 255])
+            Window.draw_font(44, 404, "うん、やっぱり似合うね。", font, color:[165, 83, 126])
             Window.draw_font(0, 0, "好感度：#{likability}", font, color:[255, 255, 255])
         end
 
@@ -178,12 +181,12 @@ Window.loop do
             Window.draw_font(135, 229, "私も中野くんにプレゼント用意してたの。手袋だよ。", font, color:[165, 83, 126])
             # 選択肢2
             Window.draw_scale(109, 274, button_img, 0.6, 0.6, 0, 0)
-            Window.draw_font(225, 279, "可愛いね。気に入ったよ。", font, color:[165, 83, 126])
+            Window.draw_font(200, 279, "もっと豪華なのが良かったなぁ…", font, color:[165, 83, 126])
             # 選択肢3
             Window.draw_scale(109, 324, button_img, 0.6, 0.6, 0, 0)
-            Window.draw_font(200, 329, "もっと豪華なのが良かったなぁ…", font, color:[165, 83, 126])
+            Window.draw_font(225, 329, "可愛いね。気に入ったよ。", font, color:[165, 83, 126])
 
-            if timer >= 45
+            if timer >= 40
                 # マウスの座標が特定の領域にある間、選択肢をホバー状態にする
                 if (109 < x && x <541 && 224 < y && y < 260) then # 選択肢1
                     Window.draw_scale(109, 224, button_hover_img, 0.6, 0.6, 0, 0)
@@ -196,20 +199,20 @@ Window.loop do
                     end
                 elsif (109 < x && x <541 && 274 < y && y < 310) then # 選択肢2
                     Window.draw_scale(109, 274, button_hover_img, 0.6, 0.6, 0, 0)
-                    Window.draw_font(225, 279, "可愛いね。気に入ったよ。", font, color:[255, 255, 255])
+                    Window.draw_font(200, 279, "もっと豪華なのが良かったなぁ…", font, color:[255, 255, 255])
                     if Input.mouse_push?(M_LBUTTON) then
                         player_chose = 2
                         flag = 0
                         flag_end = 1
+                        likability -= 1
                     end
                 elsif (109 < x && x <541 && 324 < y && y < 360) then # 選択肢3（バットエンド）
                     Window.draw_scale(109, 324, button_hover_img, 0.6, 0.6, 0, 0)
-                    Window.draw_font(200, 329, "もっと豪華なのが良かったなぁ…", font, color:[255, 255, 255])
+                    Window.draw_font(225, 329, "可愛いね。気に入ったよ。", font, color:[255, 255, 255])
                     if Input.mouse_push?(M_LBUTTON) then
                         player_chose = 3
                         flag = 0
                         flag_end = 1
-                        likability -= 1
                     end
                 end
             end
